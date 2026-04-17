97b76fb947af68c0e0df11dfdfc8ada0bd22fbbb
Daniil Skibitskyi <danchik000g@gmail.com>
2026-04-18 02:11:58 +0300

    feat: add Question and Answer models

--- qa/models.py
+++ qa/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    body = models.TextField()
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+
+    question = models.ForeignKey(
+        Question,
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+
+    is_accepted = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Answer to: {self.question.title}"
