71eb7709a20eb6d755b3fc0f35ab1a03a65e1506
Daniil Skibitskyi <danchik000g@gmail.com>
2026-04-18 02:18:53 +0300

    feat: add admin panel for Question and Answer

--- qa/admin.py
+++ qa/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Question, Answer
 
-# Register your models here.
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "created_at", "updated_at")
+    search_fields = ("title", "body", "author__username")
+    list_filter = ("created_at", "updated_at")
+    ordering = ("-created_at",)
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("id", "question", "author", "is_accepted", "created_at", "updated_at")
+    search_fields = ("body", "author__username", "question__title")
+    list_filter = ("is_accepted", "created_at")
+    ordering = ("-created_at",)
